e3a81f7b582ff91a79c2b9ca6a98d64fcebdbe0e
prem0132 <prem.it.rits@gmail.com>
2019-04-07 22:39:26 +0530

    dir to copy

--- pkg/utils/path.go
+++ pkg/utils/path.go
@@ -177,10 +177,11 @@ func PathFromK8sToDstKeySpace(k8sPath, cassandraDataDir, dstBasePath string) str
 	tableWithHash := pSplit[4]
 	// 5 = snapshots
 	//tag := pSplit[6]
-	file := pSplit[5]
-
-	if &pSplit[6] == nil {
-		file = file + pSplit[6]
+	file := ""
+	if len(k8sPath) < 6 {
+		file = pSplit[5]
+	} else {
+		file = pSplit[5] + "/" + pSplit[6]
 	}
 
 	table := strings.Split(tableWithHash, "-")[0]
